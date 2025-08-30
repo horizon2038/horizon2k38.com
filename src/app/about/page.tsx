faa55a030d1b61a0cb909d501fbb77d0ddab370a
horizon <rekka728@gmail.com>
2025-08-31 06:30:38 +0900

    [update] about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -24,10 +24,31 @@ export default function AboutPage() {
                 </ul>
                 <h2>Afflications</h2>
                 <ul>
+                    <li>2023-05 - 2023-11: Creator, <a href="https://jr.mitou.org/projects/2023/a9n">MITOU Jr Program</a></li>
                     <li>2024-06 - Present: Member, <a href="https://diver-x.jp/">Diver-X, Inc.</a></li>
+                    <li>2024-06 - 2025-03: Creator, <a href="https://www.ipa.go.jp/jinzai/mitou/it/2024/gaiyou-sg-2.html">MITOU IT Program</a></li>
                     <li>2024-07 - Present: Member, <a href="https://mitou.org/">MITOU Foundation</a></li>
                     <li>2025-04 - Present: Undergraduate Student, <a href="https://www.sfc.keio.ac.jp/">Faculty of Environment and Information Studies, Keio University</a></li>
                     <li>2025-04 - Present: Member, <a href="https://rg.sfc.keio.ac.jp/">SFC-RG</a> VIRT/<a href="https://arch.sfc.wide.ad.jp/">Arch</a> (Kusumoto/Takashio/Van Meter/Uehara/Mitsugi/Nakazawa/Okoshi Joint Research Group)</li>
+                </ul>
+                <h2>Certifiate</h2>
+                <ul>
+                    <li>
+                        2023-12: <a href="https://jr.mitou.org/projects/2023">&apos;23 MITOU Jr Super-Creator</a>
+                        <ul>
+                            <li><i>A9N - A Microkernel Achieving Portability Using HAL</i></li>
+                            <li>The presentation video is available <a href="https://youtu.be/VJOOFltOJJk?si=LBfk2IhD_jKtejJa">here</a></li>
+                            <li><a href="https://prtimes.jp/main/html/rd/p/000000014.000022934.html">Press Release</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        2025-05: <a href="https://www.ipa.go.jp/jinzai/mitou/koubo/career/2024/2024-supercreator-14.html">&apos;24 MITOU IT Super-Creator</a>
+                        <ul>
+                            <li><i>Secure User-Level Memory Management System with a Capability-Based Microkernel</i></li>
+                            <li>The presentation video is available <a href="https://youtu.be/YG5ikc-X_Kg?si=bVjdbYTw5z3Zx7Pl">here</a></li>
+                            <li><a href="https://www.meti.go.jp/press/2025/05/20250529001/20250529001.html">Press Release</a></li>
+                        </ul>
+                    </li>
                 </ul>
                 <h2>Skills</h2>
                 <ul>
@@ -40,7 +61,7 @@ export default function AboutPage() {
                             <li>Compiler</li>
                             <li>Software Architecture</li>
                             <li>Performance Optimization</li>
-                            <li>Network (Layer 3-4)</li>
+                            <li>Network (Layer 2-4)</li>
                         </ul>
                     </li>
                     <li>
@@ -61,35 +82,7 @@ export default function AboutPage() {
                     <li>Email: <a href="mailto:horizon@sfc.wide.ad.jp">horizon@sfc.wide.ad.jp</a></li>
                     <li>X: <a href="https://x.com/horizon2k38">@horizon2k38</a></li>
                     <li>Mastodon: <a href="https://mstdn.jp/@horizon2k38">@horizon2k38@mstdn.jp</a></li>
-                </ul>
-                <h2>Awards</h2>
-                <ul>
-                    <li>
-                        2023-05: <a href="https://jr.mitou.org/projects/2023/a9n">&apos;23 MITOU Jr Creator</a>
-                        <ul>
-                            <li><i>A9N - A Microkernel Achieving Portability Using HAL</i></li>
-                            <li>The presentation video is available <a href="https://youtu.be/VJOOFltOJJk?si=LBfk2IhD_jKtejJa">here</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        2023-12: <a href="https://jr.mitou.org/projects/2023">&apos;23 MITOU Jr Super-Creator</a>
-                        <ul>
-                            <li><a href="https://prtimes.jp/main/html/rd/p/000000014.000022934.html">Press Release</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        2024-06: <a href="https://www.ipa.go.jp/jinzai/mitou/it/2024/gaiyou-sg-2.html">&apos;24 MITOU IT Creator</a>
-                        <ul>
-                            <li><i>Secure User-Level Memory Management System with a Capability-Based Microkernel</i></li>
-                            <li>The presentation video is available <a href="https://youtu.be/YG5ikc-X_Kg?si=bVjdbYTw5z3Zx7Pl">here</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        2025-05: <a href="https://www.ipa.go.jp/jinzai/mitou/koubo/career/2024/2024-supercreator-14.html">&apos;24 MITOU IT Super-Creator</a>
-                        <ul>
-                            <li><a href="https://www.meti.go.jp/press/2025/05/20250529001/20250529001.html">Press Release</a></li>
-                        </ul>
-                    </li>
+                    <li>GitHub: <a href="https://github.com/horizon2038">@horizon2038</a></li>
                 </ul>
             </div>
         </div>
