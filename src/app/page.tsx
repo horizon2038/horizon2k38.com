a13b36ec46bd3cd368278fa35c728813c7879e3f
horizon <rekka728@gmail.com>
2025-08-31 23:40:52 +0900

    [fix] Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import type {Metadata} from 'next';
+import Logo from '@/components/ui/Logo';
+import Link from 'next/link';
 
 const siteUrl = process.env.SITE_URL || "https://horizon2k38.com";
 const og = `${siteUrl}/opengraph-image.png`;
@@ -30,9 +32,13 @@ export const metadata: Metadata = {
 export default function Home() {
     return (
         <div className="container mx-auto px-4 py-4 text-center">
-            <div className="text-8xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-sky-400 from- via-blue-500 via- to-indigo-800 to-">Hello, World!</div>
+            <div className="text-8xl mt-20 font-bold text-transparent bg-clip-text bg-gradient-to-r from-sky-400 from- via-blue-500 via- to-indigo-800 to-">Hello, World!</div>
             <div className="text-2xl py-4">
                 Hello, I&#39;m <span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-sky-400 from- via-blue-500 via- to-indigo-400 to-">Rekka &quot;horizon&quot; IGUMI.</span><br />
+            </div>
+            <div className="mt-5 text-xl text-center">
+                Welcome to my personal website. <br />
+                More information about me can be found on the <Link href="/about">About</Link> page.<br />
             </div>
         </div>
     );
